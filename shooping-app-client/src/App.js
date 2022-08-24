268a1b8bfa1c7fca0e5d990ea25920eca29f7c3e
Bahaa Abo Khaled <bahaa.love84@gmail.com>
2022-08-24 10:25:31 +0200

    Add navigation pages into App component

--- shooping-app-client/src/App.js
+++ shooping-app-client/src/App.js
@@ -1,10 +1,35 @@
+import { Fragment } from 'react';
 import './App.css';
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import Products from "./Pages/Products";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Footer from "./Components/Footer";
+import Login from "./Pages/Login";
+import Signup from './Pages/Signup';
+import  ErrorPage  from './Pages/ErrorPage';
+
+import {Routes,Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Fragment>
+      <div className='App'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/products' element={<Products />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/signup' element={<Signup />}></Route>
+          <Route path='*' element={<ErrorPage/>}></Route>
+          
+        </Routes>
+        <Footer/>
+      </div>
+    </Fragment>
   );
 }
 
