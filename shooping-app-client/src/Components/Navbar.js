0712a0518c6249b4c69597ec16dddae0960cbbbd
Bahaa Abo Khaled <bahaa.love84@gmail.com>
2022-08-27 14:59:21 +0200

    Add toggle state

--- shooping-app-client/src/Components/Navbar.js
+++ shooping-app-client/src/Components/Navbar.js
@@ -7,9 +7,12 @@ import { styled } from '@mui/material/styles';
 import LoginIcon from '@mui/icons-material/Login';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from 'react';
 
 
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false);
     const NavButton = styled(Button)({
         color: "#333",
         transition: "0.3s",
@@ -40,7 +43,20 @@ const Navbar = () => {
                             <NavButton href='/card' variant="outlined" startIcon={<AddShoppingCartIcon  fontSize="small"  />}>Card</NavButton>
                         </div>
                     </div>
+                    <div className={Styles.menuIcon} onClick={()=> setToggle(!toggle)}>
+                        <MenuIcon fontSize='large' />
+                    </div>
                 </nav>
+                <div className={toggle? Styles.openMegaMenu : Styles.closeMegaMenu}>
+                    <ul className={Styles.menuLinks}>
+                        <li className={Styles.menuItem}><NavLink className={Styles.textDeco} onClick={()=>setToggle(!toggle)} to="/">Home</NavLink></li>
+                        <li className={Styles.menuItem}><NavLink className={Styles.textDeco} onClick={()=>setToggle(!toggle)}to="/products">Products</NavLink></li>
+                        <li className={Styles.menuItem}><NavLink className={Styles.textDeco} onClick={()=>setToggle(!toggle)} to="/about">About</NavLink></li>
+                        <li className={Styles.menuItem}><NavLink className={Styles.textDeco} onClick={()=>setToggle(!toggle)}to="/contact">Contact</NavLink></li>
+                        <li className={Styles.menuItem}><NavLink className={Styles.textDeco} onClick={()=>setToggle(!toggle)} to="/login">Login</NavLink></li>
+                        <li className={Styles.menuItem}><NavLink className={Styles.textDeco} onClick={()=>setToggle(!toggle)} to="/signup">Signup</NavLink></li>
+                    </ul>
+                </div>
             </div>
         </section>
     )
