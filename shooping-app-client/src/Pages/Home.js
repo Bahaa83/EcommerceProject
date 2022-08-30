809d9e4bec1590e630b145e45fe8d988d50b81a8
Bahaa Abo Khaled <bahaa.love84@gmail.com>
2022-08-31 01:37:25 +0200

    Add header to home component

--- shooping-app-client/src/Pages/Home.js
+++ shooping-app-client/src/Pages/Home.js
@@ -1,11 +1,18 @@
 import React, { Fragment } from 'react';
 import LandingPage from "./LandingPage";
+import "../App.css";
+import Styles from "../Modules/Home.module.css";
 
 const Home = () => {
     return (
         <Fragment>
             
-                <LandingPage/>
+            <LandingPage />
+            <div className="container">
+                <div id="Products" className={Styles.header}>
+                    <h2>Latest Products</h2>
+                </div>
+            </div>
             
         </Fragment>
     )
