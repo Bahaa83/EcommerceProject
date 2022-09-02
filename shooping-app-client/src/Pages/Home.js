f5d855288eca4f5856093a6d720840f65200143d
Bahaa Abo Khaled <bahaa.love84@gmail.com>
2022-09-02 10:28:44 +0200

    Fitch data using useEEfect

--- shooping-app-client/src/Pages/Home.js
+++ shooping-app-client/src/Pages/Home.js
@@ -1,9 +1,24 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import LandingPage from "./LandingPage";
 import "../App.css";
 import Styles from "../Modules/Home.module.css";
+import FilterTool from '../Components/FilterTool';
+import ProductsList from "../Components/ProductsList";
+import axios from "axios";
+import { useState } from 'react';
 
 const Home = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fitchData();
+    }, [])
+    
+    const fitchData =  () => {
+        axios.get("http://localhost:3004/products")
+            .then(resp => setProducts(resp.data))
+            .catch(erro => console.log(erro))
+            .finally(console.log(products))
+    }
     return (
         <Fragment>
             
@@ -12,6 +27,9 @@ const Home = () => {
                 <div id="Products" className={Styles.header}>
                     <h2>Latest Products</h2>
                 </div>
+                <FilterTool />
+                <ProductsList products={products}/>
+                
             </div>
             
         </Fragment>
