17737a485f8ba96e95223cec6f913ce77ef6f703
Bahaa Abo Khaled <bahaa.love84@gmail.com>
2022-09-03 14:26:36 +0200

    Add filter component into Productslist component

--- shooping-app-client/src/Components/ProductsList.js
+++ shooping-app-client/src/Components/ProductsList.js
@@ -2,8 +2,9 @@ import React, { Fragment } from 'react';
 import Product from "./Product";
 import "../App.css";
 import Styles from "../Modules/ProductsList.module.css";
+import FilterTool from './FilterTool';
 
-const ProductsList = ({ products }) => {
+const ProductsList = ({ products,filterHandller }) => {
     console.log(products)
     const productlist = products.map((product) => (
             
@@ -12,6 +13,10 @@ const ProductsList = ({ products }) => {
 return (
     <Fragment>
         <div className='container'>
+            <div className={Styles.header}>
+                <h2>Latest Products</h2>
+            </div>
+            <FilterTool filterHandller ={filterHandller} />
             <div className={Styles.content}>
                 {productlist}
             </div>
