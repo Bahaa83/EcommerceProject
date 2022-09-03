e29a655a2bbd28eba07b9910fff3e614910f50bc
Bahaa Abo Khaled <bahaa.love84@gmail.com>
2022-09-03 14:24:50 +0200

    Implement fitch data and filter products

--- shooping-app-client/src/Pages/Home.js
+++ shooping-app-client/src/Pages/Home.js
@@ -1,35 +1,45 @@
 import React, { Fragment, useEffect } from 'react';
 import LandingPage from "./LandingPage";
 import "../App.css";
-import Styles from "../Modules/Home.module.css";
-import FilterTool from '../Components/FilterTool';
 import ProductsList from "../Components/ProductsList";
 import axios from "axios";
 import { useState } from 'react';
 
 const Home = () => {
     const [products, setProducts] = useState([]);
+    const [filter, setFilter] = useState([]);
     useEffect(() => {
         fitchData();
     }, [])
     
     const fitchData =  () => {
         axios.get("http://localhost:3004/products")
-            .then(resp => setProducts(resp.data))
+            .then(resp => {
+                setProducts(resp.data)
+                setFilter(resp.data)
+            })
             .catch(erro => console.log(erro))
-            .finally(console.log(products))
+            
+    }
+    const filterProducts = (keyWord) => {
+        if (keyWord === "All")
+            {
+            setFilter(products);
+            }
+        else
+            {
+            const newProducts = products.filter((item) =>
+            item.category === keyWord
+        );
+        setFilter(newProducts);
+            }
     }
     return (
         <Fragment>
             
             <LandingPage />
             <div className="container">
-                <div id="Products" className={Styles.header}>
-                    <h2>Latest Products</h2>
-                </div>
-                <FilterTool />
-                <ProductsList products={products}/>
-                
+                <ProductsList products={filter} filterHandller ={filterProducts}/>
             </div>
             
         </Fragment>
