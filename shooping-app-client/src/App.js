0e372415ed7a353ffd266f0d8f2d45c761098b96
Bahaa Abo Khaled <bahaa.love84@gmail.com>
2022-09-06 09:37:03 +0200

    Import card component to App component

--- shooping-app-client/src/App.js
+++ shooping-app-client/src/App.js
@@ -10,9 +10,8 @@ import Login from "./Pages/Login";
 import Signup from './Pages/Signup';
 import ErrorPage from './Pages/ErrorPage';
 import Details from "./Pages/Details";
-
-
 import {Routes,Route } from "react-router-dom";
+import Card  from './Pages/Card';
 
 function App() {
   return (
@@ -26,7 +25,8 @@ function App() {
           <Route path='/contact' element={<Contact />}></Route>
           <Route path='/login' element={<Login />}></Route>
           <Route path='/signup' element={<Signup />}></Route>
-          <Route path='/details/:id' element={<Details/>}></Route>
+          <Route path='/details/:id' element={<Details />}></Route>
+          <Route path='/card' element={<Card/>}></Route>
           <Route path='*' element={<ErrorPage/>}></Route>
           
         </Routes>
