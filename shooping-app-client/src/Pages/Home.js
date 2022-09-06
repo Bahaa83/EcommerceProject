e6f3ee8a728c8dba24f5c71919d7d7d52da6a3c9
Bahaa Abo Khaled <bahaa.love84@gmail.com>
2022-09-06 11:42:51 +0200

    Code optimization

--- shooping-app-client/src/Pages/Home.js
+++ shooping-app-client/src/Pages/Home.js
@@ -1,47 +1,11 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment} from 'react';
 import LandingPage from "./LandingPage";
 import "../App.css";
-import ProductsList from "../Components/ProductsList";
-import axios from "axios";
-import { useState } from 'react';
 
 const Home = () => {
-    const [products, setProducts] = useState([]);
-    const [filter, setFilter] = useState([]);
-    useEffect(() => {
-        fitchData();
-    }, [])
-    
-    const fitchData =  () => {
-        axios.get("http://localhost:3004/products")
-            .then(resp => {
-                setProducts(resp.data)
-                setFilter(resp.data)
-            })
-            .catch(erro => console.log(erro))
-            
-    }
-    const filterProducts = (keyWord) => {
-        if (keyWord === "All")
-            {
-            setFilter(products);
-            }
-        else
-            {
-            const newProducts = products.filter((item) =>
-            item.category === keyWord
-        );
-        setFilter(newProducts);
-            }
-    }
     return (
         <Fragment>
-            
             <LandingPage />
-            <div className="container">
-                <ProductsList products={filter} filterHandller ={filterProducts}/>
-            </div>
-            
         </Fragment>
     )
 }
